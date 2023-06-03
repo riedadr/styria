e4b42d5bfb874af4a19c1d10e3d7c1ca87dd79cc
riedadr@karsten <riedadr@gmail.com>
2023-06-03 22:57:30 +0200

    next-auth fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
 	providers: [
 		DiscordProvider({
 			clientId: process.env.DISCORD_CLIENT_ID!,
